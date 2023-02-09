908f415498599ccb3fcdc5421d11c55cf8ccbbf6
Mike <mike.blackburn.541@gmail.com>
2023-02-09 17:58:22 -0500

    first commit

---
+++ Coffe Shop Resgistrations/Controllers/HomeController.cs
@@ -0,0 +1,46 @@
+﻿using Coffe_Shop_Resgistrations.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Coffe_Shop_Resgistrations.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+        public IActionResult Menu()
+        {
+            return View();
+        }
+
+        public IActionResult Result(User s)
+        {
+            return View(s);
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Coffe Shop Resgistrations/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Coffe_Shop_Resgistrations.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
